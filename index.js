a10f0a10d91ef3a11ac52a1f50c682df23e0ff9f
Mauro <mauroschwab@MacBook.local>
2023-05-13 14:22:13 -0300

    sort referente al precio

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ let gastos = [
   {articulo: "bananas", precio: 500, fecha: "2023-05-12", detalle: "un kilo"},
   {articulo: "jugo", precio: 400, fecha: "2023-05-12", detalle: "1lt naranja"},
   {articulo: "empanadas", precio: 1500, fecha: "2023-05-12", detalle: "una docena"},
+  {articulo: "hockey stick", precio: 150000, fecha: "2023-05-12", detalle: "vblack wit"},
+  {articulo: "canilleras", precio: 3500, fecha: "2023-05-12", detalle: "un par"},
+  {articulo: "zapatillas", precio: 45000, fecha: "2023-05-12", detalle: "un par"},
+  {articulo: "office 365", precio: 9000, fecha: "2023-05-12", detalle: "licencia familiar por un año"},
+  {articulo: "MacBook Air", precio: 480000, fecha: "2023-05-12", detalle: "M1 gris espacial 😱"},
+  {articulo: "Smartphone Motorola", precio: 145000, fecha: "2023-05-12", detalle: "Moto G"},
+  
 ] //gastos puede ser un arreglo vacio inicialmente o traido desde una db.
 
-function mostrarGastos(gastos){
+const mostrarGastos = (gastos) => {
 
   const list = document.getElementById("list")
   
@@ -15,18 +22,18 @@ function mostrarGastos(gastos){
   }
 
   gastos.forEach(gasto => {
-    const item = document.createElement("span")
+    const item = document.createElement("div")
 
     item.className = "card border-dark m-3 card-item"
     item.innerHTML = `<div class="card-header border-dark">${gasto.articulo}</div>
-                      <div class="card-body text-dark">
+                      <div class="card-body">
                         <h5 class="card-title">$${gasto.precio}</h5>
                         <p class="card-text">${gasto.detalle}</p>
                       </div>
-                      <div class="card-footer bg-transparent border-success">${gasto.fecha}</div>`
+                      <div class="card-footer border-dark">${gasto.fecha}</div>`
 
     if (gasto.precio >= 10000) {
-      item.className += " bg-danger"
+      item.className += " bg-danger text-white"
     } else if (gasto.precio >= 5000) {
       item.className += " bg-warning"
     }
@@ -38,7 +45,7 @@ function mostrarGastos(gastos){
 
 mostrarGastos(gastos)
 
-function esValido(){
+const esValido = () => {
   if (document.getElementById("articulo").value === "" || document.getElementById("precio").value === "" || document.getElementById("fecha").value === ""  ){
     alert("Debe rellenar los campos Articulo, Precio y Fecha al menos.")
     return false
@@ -47,7 +54,7 @@ function esValido(){
   }
 }
 
-function eventoAgregar(event) {
+const eventoAgregar = (event) => {
 
   if (esValido()) {
     const gasto = {
@@ -70,8 +77,9 @@ function eventoAgregar(event) {
 }
 document.getElementById("botonAgregar").onclick = eventoAgregar
 
-function eventoFiltrarPorPrecio(event) {
-  mostrarGastos(gastos.filter(g => g.precio >= document.getElementById("price").value))
+const eventoFiltrarPorPrecio = (event) => {
+  const p = parseInt(document.getElementById("price").value)
+  mostrarGastos(gastos.filter(g => g.precio >= p))
 }
 document.getElementById("botonFiltrarPrecio").onclick = eventoFiltrarPorPrecio
 
@@ -81,4 +89,24 @@ const eventoLimpiarFiltroPrecio = (event) => {
 }
 document.getElementById("botonLimpiarFiltroPrecio").onclick = eventoLimpiarFiltroPrecio
 
-console.log(gastos);
+const precioSort = (a, b) =>  {
+  // 
+  if (a.precio > b.precio) {
+    return 1
+  }
+  if (a.precio < b.precio){
+    return -1
+  }
+  return 0
+}
+
+const eventoSortPrecio = (evento) => {
+  const copia = [...gastos]
+  mostrarGastos(copia.sort(precioSort))
+}
+document.getElementById("botonSortPrecio").onclick = eventoSortPrecio
+
+const eventoRestaurarOrden = (evento) => {
+  mostrarGastos(gastos)
+}
+document.getElementById("botonRestaurar").onclick = eventoRestaurarOrden

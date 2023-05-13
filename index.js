6d6bbfede8a787570ef85051badde5101d6a627d
Mauro <mauroschwab@MacBook.local>
2023-05-13 06:37:52 -0300

    inicio de gastos

--- index.js
+++ index.js
@@ -1,31 +1,84 @@
-// let nombre = prompt("Cual es su nombre?")
+let gastos = [
+  {articulo: "galletitas", precio: 350, fecha: "2023-05-12", detalle: "traviata"},
+  {articulo: "bananas", precio: 500, fecha: "2023-05-12", detalle: "un kilo"},
+  {articulo: "jugo", precio: 400, fecha: "2023-05-12", detalle: "1lt naranja"},
+  {articulo: "empanadas", precio: 1500, fecha: "2023-05-12", detalle: "una docena"},
+] //gastos puede ser un arreglo vacio inicialmente o traido desde una db.
 
-// if (nombres.includes(nombre.toLowerCase())) {
-// 	alert("Su nombre, "+ nombre +", se encuentra dentro del ranking de los 1000 nombres más utilizados.")
-// } else {
-// 	alert("Hola, su nombre: " + nombre + ", no es muy común.")
-// }
+function mostrarGastos(gastos){
 
-// document.write("<h2>Lo que sigue se imprime con un bucle while.</h2>")
-// let i = 0
-// while (i < 5) {
-// 	document.write("<p>Éste es el párrafo número " + i +".</p>")
-// 	i++
-// }
+  const list = document.getElementById("list")
+  
+  //Vaciamos el contenido del del div "list"
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild)    
+  }
 
+  gastos.forEach(gasto => {
+    const item = document.createElement("span")
 
-let prod_array = [
-    {id: "0", art: "a", precio:"10", desc: "Es el producto a", imagen: ""},
-    {id: "1", art: "b", precio:"15", desc: "ES un producto b", imagen: ""},
-    {id: "2", art: "c", precio:"12", desc: "c es una division", imagen: ""},
-    {id: "3", art: "d", precio:"40", desc: "d es el item 3", imagen: ""},
-    {id: "4", art: "e", precio:"35", desc: "e, solo e", imagen: ""}
-]
+    item.className = "card border-dark m-3 card-item"
+    item.innerHTML = `<div class="card-header border-dark">${gasto.articulo}</div>
+                      <div class="card-body text-dark">
+                        <h5 class="card-title">$${gasto.precio}</h5>
+                        <p class="card-text">${gasto.detalle}</p>
+                      </div>
+                      <div class="card-footer bg-transparent border-success">${gasto.fecha}</div>`
 
-let container = document.getElementById("container")
+    if (gasto.precio >= 10000) {
+      item.className += " bg-danger"
+    } else if (gasto.precio >= 5000) {
+      item.className += " bg-warning"
+    }
 
-prod_array.forEach(item => {
-    let div = document.createElement("div")
-    div.innerHTML = `<span>Artículo: ${item.art} Precio: ${item.precio}</span><hr>`
-    container.append(div)
-})
+    list.append(item)
+  })
+
+}
+
+mostrarGastos(gastos)
+
+function esValido(){
+  if (document.getElementById("articulo").value === "" || document.getElementById("precio").value === "" || document.getElementById("fecha").value === ""  ){
+    alert("Debe rellenar los campos Articulo, Precio y Fecha al menos.")
+    return false
+  } else {
+    return true
+  }
+}
+
+function eventoAgregar(event) {
+
+  if (esValido()) {
+    const gasto = {
+      articulo: articulo.value,
+      precio: precio.value,
+      fecha: fecha.value,
+      detalle: detalle.value
+    }
+
+    gastos.push(gasto)
+  
+    articulo.value = ""
+    precio.value = ""
+    fecha.value = ""
+    detalle.value = ""
+  
+    mostrarGastos(gastos)
+  }
+
+}
+document.getElementById("botonAgregar").onclick = eventoAgregar
+
+function eventoFiltrarPorPrecio(event) {
+  mostrarGastos(gastos.filter(g => g.precio >= document.getElementById("price").value))
+}
+document.getElementById("botonFiltrarPrecio").onclick = eventoFiltrarPorPrecio
+
+const eventoLimpiarFiltroPrecio = (event) => {
+  let price = document.getElementById("price").value = ""
+  mostrarGastos(gastos)
+}
+document.getElementById("botonLimpiarFiltroPrecio").onclick = eventoLimpiarFiltroPrecio
+
+console.log(gastos);

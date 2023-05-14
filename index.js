aa46b588080fe144d400584c125a1b5e18166573
Mauro <mauroschwab@MacBook.local>
2023-05-14 09:00:36 -0300

    orden y busqueda por fecha

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 let gastos = [
   {articulo: "galletitas", precio: 350, fecha: "2023-05-12", detalle: "traviata"},
   {articulo: "bananas", precio: 500, fecha: "2023-05-12", detalle: "un kilo"},
-  {articulo: "jugo", precio: 400, fecha: "2023-05-12", detalle: "1lt naranja"},
-  {articulo: "empanadas", precio: 1500, fecha: "2023-05-12", detalle: "una docena"},
+  {articulo: "jugo", precio: 400, fecha: "2023-05-14", detalle: "1lt naranja"},
+  {articulo: "empanadas", precio: 1500, fecha: "2023-05-10", detalle: "una docena"},
   {articulo: "hockey stick", precio: 150000, fecha: "2023-05-12", detalle: "vblack wit"},
   {articulo: "canilleras", precio: 3500, fecha: "2023-05-12", detalle: "un par"},
   {articulo: "zapatillas", precio: 45000, fecha: "2023-05-12", detalle: "un par"},
   {articulo: "office 365", precio: 9000, fecha: "2023-05-12", detalle: "licencia familiar por un año"},
   {articulo: "MacBook Air", precio: 480000, fecha: "2023-05-12", detalle: "M1 gris espacial 😱"},
-  {articulo: "Smartphone Motorola", precio: 145000, fecha: "2023-05-12", detalle: "Moto G"},
-  
+  {articulo: "Smartphone Motorola", precio: 145000, fecha: "2023-05-12", detalle: "Moto G"}
 ] //gastos puede ser un arreglo vacio inicialmente o traido desde una db.
+
+let aux = [...gastos]
 
 const mostrarGastos = (gastos) => {
 
@@ -21,26 +22,35 @@ const mostrarGastos = (gastos) => {
     list.removeChild(list.firstChild)    
   }
 
-  gastos.forEach(gasto => {
+  if (gastos.length > 0){
+    gastos.forEach(gasto => {
+      const item = document.createElement("div")
+  
+      item.className = "card border-dark m-3 card-item"
+      item.innerHTML = `<div class="card-header border-dark">${gasto.articulo}</div>
+                        <div class="card-body">
+                          <h5 class="card-title">$${gasto.precio}</h5>
+                          <p class="card-text">${gasto.detalle}</p>
+                        </div>
+                        <div class="card-footer border-dark">${gasto.fecha}</div>`
+  
+      if (gasto.precio >= 10000) {
+        item.className += " bg-danger text-white"
+      } else if (gasto.precio >= 5000) {
+        item.className += " bg-warning"
+      }
+
+      list.className = "card-list"
+      list.append(item)
+    })
+  
+  } else {
     const item = document.createElement("div")
-
-    item.className = "card border-dark m-3 card-item"
-    item.innerHTML = `<div class="card-header border-dark">${gasto.articulo}</div>
-                      <div class="card-body">
-                        <h5 class="card-title">$${gasto.precio}</h5>
-                        <p class="card-text">${gasto.detalle}</p>
-                      </div>
-                      <div class="card-footer border-dark">${gasto.fecha}</div>`
-
-    if (gasto.precio >= 10000) {
-      item.className += " bg-danger text-white"
-    } else if (gasto.precio >= 5000) {
-      item.className += " bg-warning"
-    }
-
+    list.className = "card-empty"
+    item.className = "no-list"
+    item.innerHTML = "<p>👀</p>"
     list.append(item)
-  })
-
+  }
 }
 
 mostrarGastos(gastos)
@@ -54,7 +64,7 @@ const esValido = () => {
   }
 }
 
-const eventoAgregar = (event) => {
+const agregarGasto = () => {
 
   if (esValido()) {
     const gasto = {
@@ -72,25 +82,34 @@ const eventoAgregar = (event) => {
     detalle.value = ""
   
     mostrarGastos(gastos)
+    aux = [...gastos]
   }
 
 }
-document.getElementById("botonAgregar").onclick = eventoAgregar
+document.getElementById("botonAgregar").onclick = agregarGasto
 
-const eventoFiltrarPorPrecio = (event) => {
+const filtrarPorPrecioMayor = (event) => {
   const p = parseInt(document.getElementById("price").value)
-  mostrarGastos(gastos.filter(g => g.precio >= p))
+  aux = aux.filter(g => g.precio > p)
+  mostrarGastos(aux)
 }
-document.getElementById("botonFiltrarPrecio").onclick = eventoFiltrarPorPrecio
+document.getElementById("botonFiltrarPrecioMayor").onclick = filtrarPorPrecioMayor
 
-const eventoLimpiarFiltroPrecio = (event) => {
+const filtrarPorPrecioMenor = () => {
+  const p = parseInt(document.getElementById("price").value)
+  aux = aux.filter(g => g.precio < p)
+  mostrarGastos(aux)
+}
+document.getElementById("botonFiltrarPrecioMenor").onclick = filtrarPorPrecioMenor
+
+const limpiarFiltroPrecio = () => {
   let price = document.getElementById("price").value = ""
+  aux = [...gastos]
   mostrarGastos(gastos)
 }
-document.getElementById("botonLimpiarFiltroPrecio").onclick = eventoLimpiarFiltroPrecio
+document.getElementById("botonLimpiarFiltroPrecio").onclick = limpiarFiltroPrecio
 
-const precioSort = (a, b) =>  {
-  // 
+const precioSortAscendente = (a, b) =>  { 
   if (a.precio > b.precio) {
     return 1
   }
@@ -100,13 +119,74 @@ const precioSort = (a, b) =>  {
   return 0
 }
 
-const eventoSortPrecio = (evento) => {
-  const copia = [...gastos]
-  mostrarGastos(copia.sort(precioSort))
+const sortPrecioAscendente = () => {
+  aux.sort(precioSortAscendente)
+  mostrarGastos(aux)
 }
-document.getElementById("botonSortPrecio").onclick = eventoSortPrecio
+document.getElementById("botonSortPrecioAscendente").onclick = sortPrecioAscendente
 
-const eventoRestaurarOrden = (evento) => {
+const precioSortDescendente = (a, b) =>  {
+  if (a.precio > b.precio) {
+    return -1
+  }
+  if (a.precio < b.precio){
+    return 1
+  }
+  return 0
+}
+
+const sortPrecioDescendente = () => {
+  aux.sort(precioSortDescendente)
+  mostrarGastos(aux)
+}
+document.getElementById("botonSortPrecioDescendente").onclick = sortPrecioDescendente
+
+const restaurarOrden = () => {
+  aux = [...gastos]
   mostrarGastos(gastos)
 }
-document.getElementById("botonRestaurar").onclick = eventoRestaurarOrden
+document.getElementById("botonRestaurarPrecio").onclick = restaurarOrden
+document.getElementById("botonRestaurarFecha").onclick = restaurarOrden
+document.getElementById("botonRestaurarTodos").onclick = restaurarOrden
+
+const fechaSortAscendente = (a, b) => {
+  const da = new Date(a.fecha)
+  const db = new Date(b.fecha)
+  if (da > db) {
+    return 1
+  }
+  if (da < db){
+    return -1
+  }
+  return 0
+}
+
+const sortFechaAscendente = () => {
+  aux.sort(fechaSortAscendente)
+  mostrarGastos(aux)
+}
+document.getElementById("botonSortFechaAscendente").onclick = sortFechaAscendente
+
+const fechaSortDescendente = (a, b) => {
+  const da = new Date(a.fecha)
+  const db = new Date(b.fecha)
+  if (da > db) {
+    return -1
+  }
+  if (da < db){
+    return 1
+  }
+  return 0
+}
+
+const sortFechaDescendente = () => {
+  aux.sort(fechaSortDescendente)
+  mostrarGastos(aux)
+}
+document.getElementById("botonSortFechaDescendente").onclick = sortFechaDescendente
+
+const buscarFecha = () => {
+  aux = aux.filter(g => g.fecha === document.getElementById("fechaInput").value)
+  mostrarGastos( aux )
+}
+document.getElementById("botonBuscarFecha").onclick = buscarFecha
